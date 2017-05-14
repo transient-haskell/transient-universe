246a5d962e40b3ebebdecd17f1148a9f1e76322f
Harendra Kumar <harendra.kumar@gmail.com>
2017-05-14 09:17:30 +0530

    Add overview of the package and the monad

--- src/Transient/Move.hs
+++ src/Transient/Move.hs
@@ -8,7 +8,39 @@
 -- Stability   :
 -- Portability :
 --
--- | see <https://www.fpcomplete.com/user/agocorona/moving-haskell-processes-between-nodes-transient-effects-iv>
+-- | @transient-universe@ extends the seamless composability of concurrent
+-- multi-threaded programs provided by
+-- <https://github.com/transient-haskell/transient transient>
+-- to a multi-node cloud.  Distributed concurrent programs are created and
+-- composed seamlessly and effortlessly as if they were written for a single
+-- node.  @transient-universe@ has diverse applications from simple distributed
+-- applications to massively parallel and distributed map-reduce problems.  If
+-- you are considering Apache Spark or Cloud Haskell then transient might be a
+-- simpler yet better solution for you
+--
+-- Transient makes it easy to write composable, distributed event driven
+-- reactive UI applications with client side and server side code composed
+-- freely in the same application. For example,
+-- <https://hackage.haskell.org/package/axiom Axiom> is a transient based
+-- unified client and server side web application framework that provides a
+-- better programming model and composability compared to frameworks like
+-- ReactJS.
+--
+-- = Overview
+--
+-- The 'Cloud' monad adds the following facilities to complement the 'TransIO'
+-- monad:
+--
+-- * Create a distributed compute cluster of nodes
+-- * Move computations across nodes at any point during computation
+-- * Run computations on multiple nodes in parallel
+--
+-- = Further Reading
+--
+-- * <https://github.com/transient-haskell/transient/wiki/Transient-tutorial Tutorial>
+-- * <https://github.com/transient-haskell/transient-examples Examples>
+-- * <https://www.fpcomplete.com/user/agocorona/moving-haskell-processes-between-nodes-transient-effects-iv Blog post>
+--
 -----------------------------------------------------------------------------
 {-# LANGUAGE CPP #-}
 
