34a88bbe752dcb0a516e8bfb7ed2a308a0711c53
agocorona <agocorona@gmail.com>
2018-09-03 08:34:25 +0000

    tests for services

--- tests/testService.hs
+++ tests/testService.hs
@@ -1,62 +1,96 @@
-{-# LANGUAGE   CPP #-}
+{-# LANGUAGE   CPP, ScopedTypeVariables #-}
 
 
-import Transient.Base
---import Transient.Internals((!>))
-import Transient.Move
+import Transient.Internals
+import Transient.Move.Internals
 import Transient.Move.Utils
 import Transient.Logged
 import Transient.Move.Services
 import Control.Applicative
 import Control.Monad
+
 import Data.Typeable
 import Data.IORef
 import Control.Concurrent (threadDelay)
-import Control.Monad.IO.Class
+import Control.Monad.State
+import Control.Exception(SomeException,ErrorCall,throw)
 
-#ifdef Library
-#else
+main= keep $    initNode $   
+    ping1 <|> ping2  <|> singleExec <|> stream <|> failThreeTimes <|> many1
 
-clientStub params= do
-      r <- callService  "" ("service","service") params
-      lliftIO $ print (r :: String)
-
-
-
-main= keep $ runCloud $ do
-    runEmbeddedService ("service","service") serviceImplementation
-    empty
-  <|> do
---      runTestNodes [2001]
---      local $ option "start1" "start1"
-      clientStub ("hello","world")
-      empty
-
-
-
---addService s= do
---   con@Connection{myNode= mynode} <- getSData   <|> error "connection not set. please initialize it"
---
---   let mynode'= mynode{nodeServices= s:nodeServices mynode}
---   addNodes [mynode']
---   setData con{myNode= mynode'}
+         
+ping1 = do
+        local $ option "ping1" "ping monitor (must have been started"
+        r <- callService' "" monitorNode ()
+        localIO $ print (r :: ())
+        
+        
+ping2 = do
+        local $ option "ping" "ping two executors, must return: [((),())]"
+        ns <- requestInstance "" executorService 2
+        r <- mapM ping ns
+        localIO $ print r
+          
+singleExec= do
+        local $ option "single" "execution of \"ls -al\" in a executor process"
+        r <- networkExecute "" "ls -al" ""
+        localIO $ print ("RESULT",r)
 
 
 
 
-serviceImplementation :: (String,String) -> Cloud String
-serviceImplementation (x,y)= do
-      lliftIO $ print x
-      return y
+stream= do
+          local $ option "stream"  "start a remote shell with the executor, then executes different command inputs and stream results"
+          r <- shellExec <|> inputs
+          localIO $ print ("LINE",r)
+  where
+  shellExec=  networkExecuteStream "" "bash"
+
+          
+  inputs= do
+        command <- local $ do
+           option "send" "send to the remote shell"
+           input (const True) "command"
+        sendExecuteStream "" "bash" command
+        empty
+        
+        
+failThreeTimes=  do
+    local $ option "fail"  "fail"
+    
+ 
+    retries <- onAll $ liftIO $ newIORef (0 :: Int)
+    
+    let retry3 e=  do
+         liftIO $ print e
+         n <- liftIO $ atomicModifyIORef retries $ \n -> (n+1,n+1)
+         liftIO $ print ("NUMBER OF RETRIES",n)
+         if n < 3 then continue else  do
+               liftIO $ print "failed after three retries"
+               empty
+
+    local $ onException $ \(e :: CloudException) -> retry3 e
+
+    r <- networkExecute "" "UNKNOWN COMMAND" ""
+
+    localIO $ print ("LINE=",r :: String )
 
 
---service' params= wormhole undefined . loggedc $ do
---      (x,y) <- local $ return params
---      lliftIO $ print x
---      local $ return y
---      teleport
---      empty
-
-#endif
-
-
+ 
+many1=  do
+        local $ option "many" "show how a command is executed in different executor instances"
+        requestInstance "" executorService 5
+        retries <- onAll $ liftIO $ newIORef (0 :: Int)
+    
+        let retry1 n' (CloudException node _ _ )=  do
+             liftIO $ print ("tried to execute in", node)
+             n <- liftIO $ atomicModifyIORef retries $ \n -> (n+1,n+1)
+             liftIO $ print ("NUMBER OF RETRIES",n)
+             if n < n' then continue else  do
+                   liftIO $ putStr  "stop after " >> putStr (show n) >> putStrLn "retries"
+                   empty
+                   
+        local $ onException $ \e  -> retry1 5 e       
+        networkExecute "" "unknow commnd" ""
+ 
+        return ()
