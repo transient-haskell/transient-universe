f224a90b0cf039633680058c1a9f90d68e39a869
agocorona <agocorona@gmail.com>
2019-02-07 10:21:51 +0000

    added controlNodeProcess to control sell processes under the executor

--- app/server/Transient/Move/Services/executor.hs
+++ app/server/Transient/Move/Services/executor.hs
@@ -15,7 +15,7 @@
 module Main where
 
 import Transient.Internals
-
+import Transient.Move.Services.Executor
 import Transient.Move.Internals
 import Transient.Move.Utils
 import Transient.Logged(maybeFromIDyn)
@@ -35,48 +35,130 @@ import System.IO
 import System.IO.Unsafe
 import qualified Data.Map as M
 import Data.Maybe
+import qualified Data.ByteString.Lazy.Char8   as BS
+import qualified Data.ByteString.Char8 as BSS
+import Data.String
+import Data.Time
 
 
 main = do
    putStrLn "Starting Transient Executor Service"
-   keep' $ runService executorService 3005  
+   keep $ runService executorService 3005  
                 [ serve networkExecuteStreamIt
                 , serve networkExecuteIt
-                , serve sendExecuteStreamIt]
+                , serve sendExecuteStreamIt
+                , serve receiveExecuteStreamIt
+                , serve networkExecuteStreamIt'
+                , serve getLogIt
+                , serve getProcessesIt]
                 empty 
-                
+
+getProcessesIt :: GetProcesses -> Cloud [String]
+getProcessesIt _= localIO $ do
+   map1 <- readIORef rinput
+   return $ map fst $ M.toList map1
+               
+-- | send input to a remote process initiated with `networkExecuteStream` or `networkExecuteStream'`
 sendExecuteStreamIt :: (String,String) -> Cloud ()
 sendExecuteStreamIt (cmdline, inp)= do
    localIO $ do
        map <- readIORef rinput 
        let input= fromMaybe (error "this command line has not been opened") $ M.lookup cmdline map 
        hPutStrLn input inp 
-       hFlush input
+       hFlush input 
    return ()
 
+-- receive input from a remote process initiated with `networkExecuteStream'`
+receiveExecuteStreamIt :: ReceiveExecuteStream -> Cloud String   
+receiveExecuteStreamIt (ReceiveExecuteStream expr ident)= local $ do
+     labelState ident 
+     getMailbox' ("output"++ expr)
+
+-- | execute a shell script and a input, and return all the output. Called externally by `networkExecute`
 networkExecuteIt :: (String, String, ()) -> Cloud String
 networkExecuteIt (expr, input,()) = localIO $ readCreateProcess (shell expr) input
 
+getLogIt :: GetLogCmd -> Cloud BS.ByteString
+getLogIt (GetLogCmd cmd)= localIO $ BS.readFile $ logFileName cmd
 
 
+logFileName expr= subst expr ++ ".log"
+    where
+    subst []= [] 
+    subst (' ':xs)= '-':subst xs
+    subst ('/':xs)= '-':subst xs
+    subst ('\"':xs)= '-':subst xs
+    subst (x:xs)= x:subst xs
+
+networkExecuteStreamIt' :: ExecuteStream  -> Cloud String
+networkExecuteStreamIt' (ExecuteStream expr) = local $ do
+
+   setRState False
+
+   r <- executeStreamIt expr
+    
+
+
+   init <- getRState
+   if init then empty
+        else do
+           setRState True
+           return r       -- return the first output line only
+
+
+-- execute the shell command specified in a string and stream line by line the standard output/error
+-- to the service caller. It also store the output in a logfile and update a mailbox that can be
+-- inspected by `receiveExecuteStreamIt`. Invoked by `networkExecuteStream`.
+-- The first result returned is the process identifier.
 networkExecuteStreamIt :: String  -> Cloud String
-networkExecuteStreamIt expr  = local $ do
+networkExecuteStreamIt expr  =  local $ executeStreamIt expr
+
+
+
+executeStreamIt expr = do
 
       r <- liftIO $ createProcess $ (shell expr){std_in=CreatePipe,std_err=CreatePipe,std_out=CreatePipe}
-      liftIO $ atomicModifyIORef rinput $ \map -> (M.insert expr (input1 r) map,())
-      watch (output r) <|> watcherror r 
+      
+      time <- liftIO $ getCurrentTime
+      let header= expr ++"  "++ show time
+      abduce
+      labelState $ BSS.pack header
+
+      
+      onException $ \(e :: SomeException) ->  do 
+             liftIO $ do
+                 print ("watch:",e) 
+                 cleanupProcess r 
+                 atomicModifyIORef rinput $ \map -> (M.delete header  map,())
+             empty 
+      
+      let logfile= logFileName  header 
+      let box= "output" ++ header
+      liftIO $ atomicModifyIORef rinput $ \map -> (M.insert header (input1 r) map,())
+      
+      line <- async (return header) <|> watch (output r) <|> watch (err r) <|> watchExitError r 
+      
+      putMailbox' box line   
+      
+      hlog <- liftIO $ openFile logfile AppendMode 
+      liftIO $ hPutStrLn  hlog line
+      liftIO $ hClose hlog    
+      return line
       
       where
+
       input1 r= inp where (Just inp,_,_,_)= r
       output r= out where (_,Just out,_,_)= r
       err r= err where    (_,_,Just err,_)= r
       handle r= h where   (_,_,_,h)= r
 
       watch :: Handle -> TransIO String
-      watch h=    do
+      watch h = do
+        abduce
         mline  <-  threads 0 $ (parallel $  (SMore <$> hGetLine' h) `catch` \(e :: SomeException) -> return SDone)
         case mline of
            SDone -> empty
+           SError e -> do liftIO $ print ("watch:",e); empty
            SMore line ->  return line
            
         where
@@ -95,11 +177,11 @@ networkExecuteStreamIt expr  = local $ do
                    if null r then getMore buff else return r
                 else do
                       c <- hGetChar h
-                      if c== '\n' then readIORef buff else do
+                      if c == '\n' then readIORef buff else do
                         modifyIORef buff $ \str -> str ++ [c]
                         getMore buff
 
-      watcherror r= do    -- make it similar to watch
+      watchExitError r= do    -- make it similar to watch
         abduce
         liftIO $ waitForProcess $ handle r
         errors <- liftIO $  hGetContents (err r)
--- src/Transient/Move/Services/Executor.hs
+++ src/Transient/Move/Services/Executor.hs
@@ -1,11 +1,18 @@
 module Transient.Move.Services.Executor where
-
+ 
 import Transient.Internals
 import Transient.Move.Internals
 import Transient.Move.Services
 import Data.IORef
 import System.IO.Unsafe
 import qualified Data.Map as M
+import qualified Data.ByteString.Lazy.Char8   as BS
+import qualified Data.ByteString.Char8 as BSS
+import Data.String
+import Data.Typeable
+import Control.Applicative
+import Control.Monad
+import Control.Monad.State (liftIO)
     
 executorService = [("service","executor")
                   ,("executable", "executor")
@@ -17,33 +24,50 @@ executorService = [("service","executor")
 -- the number N should be less of equal than the number of phisical machines.
 -- Since the executor serivice can execute any number of processes, it sould be at most one per machine. 
 
-initExecute ident number=  requestInstance  ident executorService  number
+initExecute  number=  requestInstance   executorService  number
 
 -- | execute a command in some node by an executor service, and return the result when the program finishes
-networkExecute :: String -> String -> String ->  Cloud String
-networkExecute ident cmdline input= 
-     callService ident executorService (cmdline, input,())
+networkExecute ::  String -> String ->  Cloud String
+networkExecute  cmdline input= 
+     callService  executorService (cmdline, input,())
      
 
+
+-- | execute a process in some machine trough the local monitor and the executor.
+-- This call return a process identifier
+-- The process can be controlled with other services like `controlNodeProcess`
+networkExecuteStream'  :: String -> Cloud String
+networkExecuteStream'  cmdline= do
+    -- callService  executorService cmdline
+     node <- initService  executorService
+     return () !> ("STORED NODE", node) 
+     name <- callService' node $ ExecuteStream cmdline
+     localIO $ print ("NAME", name)
+     localIO $ atomicModifyIORef rnodecmd $ \map -> (M.insert name node map,())
+     local $ setRemoteJob (BSS.pack name) node         -- so it can be stopped by `killRemoteJob`
+     return name 
+     
 -- | execute a shell command in some node using the executor service. 
 -- The response is received as an stream of responses, one per line
-
-networkExecuteStream :: String -> String -> Cloud String      -- '[Multithreaded,Streaming]
-networkExecuteStream ident cmdline= do
-
-    -- callService ident executorService cmdline
-     node <- initService ident executorService
-     localIO $ atomicModifyIORef rnodecmd $ \map -> (M.insert cmdline node map,())
-     return () !> ("STORED NODE", node) 
-     callService' ident node cmdline
-
+networkExecuteStream :: String -> Cloud String      -- '[Multithreaded,Streaming]
+networkExecuteStream  cmdline= do
+         node <- initService  executorService
+         flag <- onAll $ liftIO $ newIORef False
+         r <- callService'  node cmdline
+         init <- onAll $ liftIO $ readIORef flag
+         when (not init) $ do
+            onAll $ liftIO $ writeIORef flag True   -- get the first line (header) as the name of the process
+            local $ setRemoteJob  (BSS.pack r) node         -- so it can be stopped by `killRemoteJob`
+            localIO $ atomicModifyIORef rnodecmd $ \map -> (M.insert r node map,())
+         return r 
+         
 rnodecmd= unsafePerformIO $ newIORef M.empty
 
 -- | send a message that will be read by the standard input of the program initiated by `networkExecuteStream`, identified by the command line.
 -- the stream of responses is returned by that primitive. `sendExecuteStream` never return anything, since it is asynchronous
-sendExecuteStream :: String -> String -> String -> Cloud  ()  -- '[Asynchronous]
-sendExecuteStream ident cmdline msg=  do
-     -- callService ident executorService (cmdline, msg) 
+sendExecuteStream :: String -> String -> Cloud  ()  -- '[Asynchronous]
+sendExecuteStream  cmdline msg=  do
+
      return () !> ("SENDEXECUTE", cmdline)
      node <- localIO $ do
        map <- readIORef rnodecmd 
@@ -52,10 +76,83 @@ sendExecuteStream ident cmdline msg=  do
          Nothing ->  error $ "sendExecuteStream: no node executing the command: "++ cmdline
          Just n -> return n
      return () !> ("NODE", node)
-     callService' ident node (cmdline, msg)
+     callService'  node (cmdline, msg)
      
      
+controlNodeProcess cmdline= do
+      exnode <-local $ do
+            map <- liftIO $ readIORef rnodecmd 
+            let mn = M.lookup cmdline map 
+            return $ case mn of
+                   Nothing ->  error $ "sendExecuteStream: no node executing the command: "++ cmdline
+                   Just n ->   n
+                   
+      send exnode  <|>  receive exnode
+      where
 
+      send exnode= do
+         local abduce
+         local $ do
+            liftIO $ writeIORef lineprocessmode True
+            oldprompt <- liftIO $ atomicModifyIORef rprompt $ \oldp -> ( takeWhile (/= ' ') cmdline++ "> ",oldp)
+            cbs <- liftIO $ atomicModifyIORef rcb $ \cbs -> ([],cbs) -- remove local node options
+            setState (oldprompt,cbs)                                             -- store them
 
+            
+         endcontrolop exnode <|> kill exnode <|> log exnode <|> inputs exnode 
+         empty
+         
+      kill exnode= do
+         local $ option "kill" "kill the process"
+         localIO $ putStrLn "process terminated"
+         killRemoteJob exnode $ fromString cmdline
+         endcontrol exnode
+         
+      endcontrolop exnode= do
+         local $ option "endcontrol"  "end controlling the process"
+         localIO $ putStrLn "end controlling the process"
+         endcontrol exnode
+         
+      endcontrol exnode= do
+         localIO $ writeIORef lineprocessmode False
+         killRemoteJob exnode  controlToken
+         local $ do
 
+            (oldprompt,cbs) <- getState
+            liftIO $ writeIORef rcb cbs -- restore local node options
+            liftIO $ writeIORef rprompt  oldprompt
+              
+      log exnode = do
+              local $ option "log" "display the log of the node"
+              log <- getLogCmd cmdline exnode
+              localIO $ do
+                 
+                 putStr "\n\n------------- LOG OF PROCESS: ">> print cmdline >> putStrLn ""
+                 mapM_ BS.putStrLn $ BS.lines log
+                 putStrLn   "------------- END OF LOG"
+                 
+      inputs exnode= do  
 
+          line <- local $ inputf False "input"  Nothing (const True)  
+          sendExecuteStream cmdline line
+         
+      
+      receive exnode=  do
+         r <- receiveExecuteStream cmdline exnode
+         when (not $ null r) $ localIO $ putStrLn  r  
+         empty
+
+      receiveExecuteStream cmd node=do
+          local $ setRemoteJob  controlToken node
+          callService'  node $ ReceiveExecuteStream cmd controlToken 
+
+      getLogCmd :: String -> Node -> Cloud BS.ByteString
+      getLogCmd cmd node= callService' node (GetLogCmd cmd)
+
+newtype GetLogCmd= GetLogCmd String  deriving (Read, Show, Typeable)
+newtype ExecuteStream= ExecuteStream String deriving (Read, Show, Typeable)
+data    ReceiveExecuteStream= ReceiveExecuteStream String BSS.ByteString deriving (Read, Show, Typeable)
+data    GetProcesses= GetProcesses deriving (Read, Show, Typeable)
+
+getProcesses ::  Node -> Cloud [String]
+getProcesses node= callService' node GetProcesses
